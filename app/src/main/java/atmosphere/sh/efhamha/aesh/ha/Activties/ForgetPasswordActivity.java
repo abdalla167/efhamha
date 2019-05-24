5aae961541ead3ce2f65fead03ef5e57810f2d4d
Youssef Essam <youssef.3ssam1@gmail.com>
2019-05-24 17:40:46 +0200

    Update Forget Password

--- app/src/main/java/atmosphere/sh/efhamha/aesh/ha/Activties/ForgetPasswordActivity.java
+++ app/src/main/java/atmosphere/sh/efhamha/aesh/ha/Activties/ForgetPasswordActivity.java
@@ -1,6 +1,7 @@
 package atmosphere.sh.efhamha.aesh.ha.Activties;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import atmosphere.sh.efhamha.aesh.ha.Helpers.InputValidator;
 import atmosphere.sh.efhamha.aesh.ha.R;
@@ -26,10 +28,13 @@ public class ForgetPasswordActivity extends AppCompatActivity {
     @BindView(R.id.btn_reset_password)
     Button btnResetPassword;
 
+
     private String email;
     private ProgressDialog progressDialog;
+
     //Firebase
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,16 +48,14 @@ public class ForgetPasswordActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_reset_password:
-                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
                 if (getInputData())
                     resetPassowrd(email);
                 break;
         }
     }
 
-    private void resetPassowrd(String email)
-    {
-        progressDialog = new ProgressDialog(getApplicationContext());
+    private void resetPassowrd(String email) {
+        progressDialog = new ProgressDialog(ForgetPasswordActivity.this);
         progressDialog.setMessage("رجاء الأنتظار....");
         progressDialog.show();
 
@@ -62,8 +65,8 @@ public class ForgetPasswordActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     progressDialog.dismiss();
                     Toast.makeText(ForgetPasswordActivity.this, "افحص بريدك الألكتروني لأعاده تعيين كلمة السر", Toast.LENGTH_SHORT).show();
-                    //startActivity(new Intent(getApplicationContext(), SignInActivity.class));
-                    //finish();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
                 } else{
                     progressDialog.dismiss();
                     Toast.makeText(ForgetPasswordActivity.this, "تعذر أرسال الأيميل" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
